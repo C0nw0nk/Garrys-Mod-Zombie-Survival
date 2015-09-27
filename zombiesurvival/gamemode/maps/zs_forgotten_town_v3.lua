5a8a9ad36f1f373a191fbec9f0aaf69774e2bc46
C0nw0nk <C0nw0nks@googlemail.com>
2015-09-27 12:29:24 +0100

    Update zs_forgotten_town_v3.lua

--- zombiesurvival/gamemode/maps/zs_forgotten_town_v3.lua
+++ zombiesurvival/gamemode/maps/zs_forgotten_town_v3.lua
@@ -1,1 +1,57 @@
+hook.Add("InitPostEntityMap", "Adding", function()
+	--Fix the props in the map to make them all usable for cading
+	for _, ent in pairs(ents.FindByClass("prop_physics")) do
+		local pro = ents.Create("prop_physics")
+		if pro:IsValid() then
+			pro:SetModel(ent:GetModel())
+			pro:SetPos(ent:GetPos())
+			pro:SetAngles(ent:GetAngles())
+			pro:Spawn()
+			ent:Remove()
+		end
+	end
+	for _, ent in pairs(ents.FindByClass("prop_physics_multiplayer")) do
+		local pro = ents.Create("prop_physics")
+		if pro:IsValid() then
+			pro:SetModel(ent:GetModel())
+			pro:SetPos(ent:GetPos())
+			pro:SetAngles(ent:GetAngles())
+			pro:Spawn()
+			ent:Remove()
+		end
+	end
+	
+	--Prevent people getting stuck in between the walls and the gas tanks
+	local ent2 = ents.Create("prop_dynamic_override")
+	if ent2:IsValid() then
+		ent2:SetPos(Vector(687, 2720, 61))
+		ent2:SetAngles(Angle(90, 0, 0))
+		ent2:SetKeyValue("solid", "6")
+		ent2:SetModel(Model("models/props_debris/wood_board06a.mdl"))
+		ent2:SetNoDraw(true)
+		ent2:Spawn()
+	end
+	local ent2 = ents.Create("prop_dynamic_override")
+	if ent2:IsValid() then
+		ent2:SetPos(Vector(586, 3023, 71))
+		ent2:SetAngles(Angle(90, 0, 90))
+		ent2:SetKeyValue("solid", "6")
+		ent2:SetModel(Model("models/props_debris/wood_board06a.mdl"))
+		ent2:SetNoDraw(true)
+		ent2:Spawn()
+	end
+	
+end)
 
+--Start credits hook
+--Credits | Please don't remove takes allot of time and effort to fix these maps
+--especially since allot of these maps are so old now and unmaintained so do
+--the right thing and give credit where it is deserved.
+--(By keeping these credits in motivates me to keep fixing maps.)
+--Removing or hiding this hook is a violation of the AGPL licensing agreement and the additional conditions. See the LICENSE.txt file that came with the script.
+hook.Add("PlayerInitialSpawn", "credits hook", function(ply)
+	timer.Simple(60, function()
+		ply:PrintMessage(HUD_PRINTTALK, "Lua map fixes by C0nw0nk")
+	end)
+end);
+--End credits hook
