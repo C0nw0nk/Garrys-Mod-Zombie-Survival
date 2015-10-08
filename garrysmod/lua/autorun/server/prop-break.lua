5a16fa15ce6b4980e9d9805c99e1d0b574050e04
C0nw0nk <C0nw0nks@googlemail.com>
2015-10-08 17:33:09 +0100

    Update prop-break.lua

--- garrysmod/lua/autorun/server/prop-break.lua
+++ garrysmod/lua/autorun/server/prop-break.lua
@@ -7,12 +7,4 @@ hook.Add("PropBroken", "PropBroken.Alert", function(ent, attacker)
 			end
 		end
 	end
-	--If player breaks a weapon output who broke the weapon to console.
-	if string.sub(ent:GetClass(), 1, 5) == "prop_weapon" then
-		if attacker:IsValid() and attacker:IsPlayer() then
-			if attacker:Team() == TEAM_HUMAN then
-				PrintTranslatedMessage(HUD_PRINTCONSOLE, " "..attacker:Name().." "..attacker:SteamID().." broke prop "..ent:GetModel().." ")
-			end
-		end
-	end
 end)
