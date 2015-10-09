1a8fd3cef8e1772f3caec4999f8d2879907e6ad3
C0nw0nk <C0nw0nks@googlemail.com>
2015-10-09 20:11:31 +0100

    Update prop-break.lua

--- garrysmod/lua/autorun/server/prop-break.lua
+++ garrysmod/lua/autorun/server/prop-break.lua
@@ -13,8 +13,16 @@ hook.Add("EntityTakeDamage", "EntityTakeDamage",  function(ent, attacker)
 	local attacker = attacker:GetAttacker()
 	--Define this to make it so that all props can not be broken 
 	--if string.match(ent:GetClass(), "prop_*") then
-	--Prevent players destroying weapons
+	--Prevent players destroying weapons.
 	if string.match(ent:GetClass(), "prop_weapon") then
+		if attacker:IsValid() and attacker:IsPlayer() then
+			if attacker:Team() == TEAM_HUMAN then
+				return true
+			end
+		end
+	end
+	--Prevent players destroying ammo.
+	if string.match(ent:GetClass(), "prop_ammo") then
 		if attacker:IsValid() and attacker:IsPlayer() then
 			if attacker:Team() == TEAM_HUMAN then
 				return true
