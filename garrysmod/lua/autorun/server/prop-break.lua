896e13c92cfc12f9ab9e133acf519d657234c144
C0nw0nk <C0nw0nks@googlemail.com>
2015-10-11 18:54:40 +0100

    Update prop-break.lua

--- garrysmod/lua/autorun/server/prop-break.lua
+++ garrysmod/lua/autorun/server/prop-break.lua
@@ -11,6 +11,54 @@ end)
 
 hook.Add("EntityTakeDamage", "EntityTakeDamage",  function(ent, attacker)
 	local attacker = attacker:GetAttacker()
+
+	--Prevent players destroying props in wave 1 or the selection of who will become a zombie.
+	--Starting wave of who will become a zombie.
+	if GetGlobalBool("wave") == false then
+		--If entity is a prop
+		if string.match(ent:GetClass(), "prop_*") then
+			--If attacker is valid and a player
+			if attacker:IsValid() and attacker:IsPlayer() then
+				--Check if attacker is on the human team
+				if attacker:Team() == TEAM_HUMAN then
+					--Check if attacker is using a melee weapon
+					--if attacker:GetActiveWeapon().IsMelee then
+						--Check that the entity prop class is not a door
+						if string.match(ent:GetClass(), "prop_door*") then
+						--Do nothing
+						else
+						--Block the damage inflicted.
+						return true
+						end
+					--end
+				end
+			end
+		end
+	else
+		--If wave is less than or equal to the first wave.
+		if GetGlobalBool("wave") <= 1 then
+			--If entity is a prop
+			if string.match(ent:GetClass(), "prop_*") then
+				--If attacker is valid and a player
+				if attacker:IsValid() and attacker:IsPlayer() then
+					--Check if attacker is on the human team
+					if attacker:Team() == TEAM_HUMAN then
+						--Check if attacker is using a melee weapon
+						--if attacker:GetActiveWeapon().IsMelee then
+							--Check that the entity prop class is not a door
+							if string.match(ent:GetClass(), "prop_door*") then
+							--Do nothing
+							else
+							--Block the damage inflicted.
+							return true
+							end
+						--end
+					end
+				end
+			end
+		end
+	end
+	
 	--Define this to make it so that all props can not be broken 
 	--if string.match(ent:GetClass(), "prop_*") then
 	--Prevent players destroying weapons.
