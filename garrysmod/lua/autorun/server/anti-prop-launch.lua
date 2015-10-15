5fbcc19bb2548733c41aed85d28454bdc607f93b
C0nw0nk <C0nw0nks@googlemail.com>
2015-10-15 13:28:04 +0100

    Update anti-prop-launch.lua

--- garrysmod/lua/autorun/server/anti-prop-launch.lua
+++ garrysmod/lua/autorun/server/anti-prop-launch.lua
@@ -9,6 +9,21 @@ hook.Add("ShouldCollide", "Prop.Collision", function(player, object)
 	if player:IsValid() and player:IsPlayer() then
 	else
 	if player:IsValid() and object:IsValid() then
+		if string.match(player:GetClass(), "prop_physics*") and string.match(object:GetClass(), "prop_arsenalcrate*") and player:GetPhysicsObject():IsPenetrating() != false and object:GetPhysicsObject():IsPenetrating() != false then
+			return false
+		end
+		if string.match(player:GetClass(), "prop_physics*") and string.match(object:GetClass(), "prop_resupplybox*") and player:GetPhysicsObject():IsPenetrating() != false and object:GetPhysicsObject():IsPenetrating() != false then
+			return false
+		end
+		if string.match(player:GetClass(), "prop_physics*") and string.match(object:GetClass(), "prop_messagebeacon*") and player:GetPhysicsObject():IsPenetrating() != false and object:GetPhysicsObject():IsPenetrating() != false then
+			return false
+		end
+		if string.match(player:GetClass(), "prop_physics*") and string.match(object:GetClass(), "prop_gunturret*") and player:GetPhysicsObject():IsPenetrating() != false and object:GetPhysicsObject():IsPenetrating() != false then
+			return false
+		end
+		if string.match(player:GetClass(), "prop_physics*") and string.match(object:GetClass(), "prop_detpack*") and player:GetPhysicsObject():IsPenetrating() != false and object:GetPhysicsObject():IsPenetrating() != false then
+			return false
+		end
 		if string.match(player:GetClass(), "prop_physics*") and string.match(object:GetClass(), "prop_spotlamp*") and player:GetPhysicsObject():IsPenetrating() != false and object:GetPhysicsObject():IsPenetrating() != false then
 			return false
 		end
