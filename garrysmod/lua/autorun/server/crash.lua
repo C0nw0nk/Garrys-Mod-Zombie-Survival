9d0326f50ace7ccaba976a69b77c5e502d577024
C0nw0nk <C0nw0nks@googlemail.com>
2015-10-15 17:07:39 +0100

    Update crash.lua

--- garrysmod/lua/autorun/server/crash.lua
+++ garrysmod/lua/autorun/server/crash.lua
@@ -1,5 +1,5 @@
-hook.Add("PlayerInitialSpawn", "anti crash hook", function(ply)
+hook.Add("PlayerConnect", "anti crash hook", function()
 	--Console command to help prevent people from crashing.
-	ply:ConCommand("r_drawmodeldecals 0")
-	ply:ConCommand("r_maxmodeldecal 50")
-end);
+	RunConsoleCommand("r_drawmodeldecals", 0)
+	RunConsoleCommand("r_maxmodeldecal", 50)
+end)
