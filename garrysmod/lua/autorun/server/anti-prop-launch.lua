30626bbc70b1f5cda22f369d01fa9628ec158f75
C0nw0nk <C0nw0nks@googlemail.com>
2015-10-16 07:16:58 +0100

    Update anti-prop-launch.lua
    Prevent doors and func_physbox props from being prop launched.

--- garrysmod/lua/autorun/server/anti-prop-launch.lua
+++ garrysmod/lua/autorun/server/anti-prop-launch.lua
@@ -1,6 +1,6 @@
 --Add collision checks to all props that are created.
 local function EntityCreated( entity )
-	if string.match(entity:GetClass(), "prop_physics*") then
+	if string.match(entity:GetClass(), "prop_physics*") or string.match(entity:GetClass(), "prop_door*") or string.match(entity:GetClass(), "func_physbox*") then
 		entity:SetCustomCollisionCheck(true)
 	end
 end
@@ -14,27 +14,27 @@ hook.Add("ShouldCollide", "Prop.Collision", function(player, object)
 	--Make sure both the prop and object its colliding with are both valid.
 	if player:IsValid() and object:IsValid() then
 		--If the prop is a prop_physics and its colliding with an arsenal crate set collision to none.
-		if string.match(player:GetClass(), "prop_physics*") and string.match(object:GetClass(), "prop_arsenalcrate*") then
+		if string.match(player:GetClass(), "prop_physics*") or string.match(entity:GetClass(), "prop_door*") or string.match(entity:GetClass(), "func_physbox*") and string.match(object:GetClass(), "prop_arsenalcrate*") then
 			return false
 		end
 		--If the prop is a prop_physics and its colliding with an resupply box set collision to none.
-		if string.match(player:GetClass(), "prop_physics*") and string.match(object:GetClass(), "prop_resupplybox*") then
+		if string.match(player:GetClass(), "prop_physics*") or string.match(entity:GetClass(), "prop_door*") or string.match(entity:GetClass(), "func_physbox*") and string.match(object:GetClass(), "prop_resupplybox*") then
 			return false
 		end
 		--If the prop is a prop_physics and its colliding with an message beacon set collision to none.
-		if string.match(player:GetClass(), "prop_physics*") and string.match(object:GetClass(), "prop_messagebeacon*") then
+		if string.match(player:GetClass(), "prop_physics*") or string.match(entity:GetClass(), "prop_door*") or string.match(entity:GetClass(), "func_physbox*") and string.match(object:GetClass(), "prop_messagebeacon*") then
 			return false
 		end
 		--If the prop is a prop_physics and its colliding with an gun turret set collision to none.
-		if string.match(player:GetClass(), "prop_physics*") and string.match(object:GetClass(), "prop_gunturret*") then
+		if string.match(player:GetClass(), "prop_physics*") or string.match(entity:GetClass(), "prop_door*") or string.match(entity:GetClass(), "func_physbox*") and string.match(object:GetClass(), "prop_gunturret*") then
 			return false
 		end
 		--If the prop is a prop_physics and its colliding with an detonation pack set collision to none.
-		if string.match(player:GetClass(), "prop_physics*") and string.match(object:GetClass(), "prop_detpack*") then
+		if string.match(player:GetClass(), "prop_physics*") or string.match(entity:GetClass(), "prop_door*") or string.match(entity:GetClass(), "func_physbox*") and string.match(object:GetClass(), "prop_detpack*") then
 			return false
 		end
 		--If the prop is a prop_physics and its colliding with an spot lamp set collision to none.
-		if string.match(player:GetClass(), "prop_physics*") and string.match(object:GetClass(), "prop_spotlamp*") then
+		if string.match(player:GetClass(), "prop_physics*") or string.match(entity:GetClass(), "prop_door*") or string.match(entity:GetClass(), "func_physbox*") and string.match(object:GetClass(), "prop_spotlamp*") then
 			return false
 		end
 	end
