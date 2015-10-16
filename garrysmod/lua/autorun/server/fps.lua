1e279432f1be4bb3b661b14fa591605d9a0b2989
C0nw0nk <C0nw0nks@googlemail.com>
2015-10-16 18:15:23 +0100

    Update fps.lua

--- garrysmod/lua/autorun/server/fps.lua
+++ garrysmod/lua/autorun/server/fps.lua
@@ -1,6 +1,6 @@
 --Help boost players fps by disabling casting of dynamic shadows.
 hook.Add("InitPostEntityMap", "Adding", function()
-  --Remove all entities with shadow_control
+  --Remove shadow_control
   for _, ent in pairs(ents.FindByClass("shadow_control")) do
     ent:Remove()
   end
