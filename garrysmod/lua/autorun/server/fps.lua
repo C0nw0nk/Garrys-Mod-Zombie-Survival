3720fd49d2cd228fb5fff94c41699e4a8b8fdb2c
C0nw0nk <C0nw0nks@googlemail.com>
2015-10-25 08:23:49 +0000

    Update fps.lua

--- garrysmod/lua/autorun/server/fps.lua
+++ garrysmod/lua/autorun/server/fps.lua
@@ -1,7 +1,22 @@
 --Help boost players fps by disabling casting of dynamic shadows.
 hook.Add("InitPostEntityMap", "Adding", function()
-  --Remove shadow_control
-  for _, ent in pairs(ents.FindByClass("shadow_control")) do
-    ent:Remove()
-  end
+	--Remove shadow_control
+	for _, ent in pairs(ents.FindByClass("shadow_control")) do
+		ent:Remove()
+	end
+end)
+--Remove shadows from player spawn.
+hook.Add("PlayerSpawn", "PlayerSpawn.Remove.Shadows", function(player)
+	player:DrawShadow(false)
+end)
+--Remove and disable shadows on props spawned in func_physbox and doors that have been taken of their hinges.
+hook.Add("Think", "Think.Remove.Shadows", function()
+	for _, ent in pairs(ents.FindByClass("prop_*")) do
+		ent:DrawShadow(false)
+		ent:SetKeyValue("disableshadows", 1)
+	end
+	for _, ent in pairs(ents.FindByClass("func_*")) do
+		ent:DrawShadow(false)
+		ent:SetKeyValue("disableshadows", 1)
+	end
 end)
