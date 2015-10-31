d00a3758a05534ae1f4a238ad5ccabbcd806007b
C0nw0nk <C0nw0nks@googlemail.com>
2015-10-31 11:12:03 +0000

    Update fps.lua
    Remove rain.

--- garrysmod/lua/autorun/server/fps.lua
+++ garrysmod/lua/autorun/server/fps.lua
@@ -4,6 +4,10 @@ hook.Add("InitPostEntityMap", "Remove.Shadow.Control", function()
 	for _, ent in pairs(ents.FindByClass("shadow_control")) do
 		ent:Remove()
 	end
+	--Remove rain.
+	for _, ent in pairs(ents.FindByClass("func_precipitation")) do
+		ent:Remove()
+	end
 end)
 --Remove shadows from players when they spawn.
 hook.Add("PlayerSpawn", "PlayerSpawn.Remove.Shadows", function(player)
