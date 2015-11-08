960e1b47dba33968c5bcf29c32093d15fd558ea0
C0nw0nk <C0nw0nks@googlemail.com>
2015-11-08 20:32:24 +0000

    Update anti-prop-launch.lua
    Add more detail.

--- garrysmod/lua/autorun/server/anti-prop-launch.lua
+++ garrysmod/lua/autorun/server/anti-prop-launch.lua
@@ -1,4 +1,5 @@
 --Prevent players getting prop killed by the class prop_physics or func_physbox
+--You can still prop kill as a zombie.
 hook.Add("EntityTakeDamage", "Anti.Prop.Launch",  function(ent, attacker)
 	local attacker = attacker:GetAttacker()
 	if string.match(attacker:GetClass(), "prop_physics*") or string.match(attacker:GetClass(), "func_physbox*") and ent:IsValid() and ent:IsPlayer() then
