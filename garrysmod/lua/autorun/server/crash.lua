aabf8bcdfa8ef955e72b1b9b0fc8a075856d5da7
C0nw0nk <C0nw0nks@googlemail.com>
2015-11-08 17:51:21 +0000

    Update crash.lua
    Fix FCVAR_SERVER_CAN_EXECUTE

--- garrysmod/lua/autorun/server/crash.lua
+++ garrysmod/lua/autorun/server/crash.lua
@@ -2,4 +2,8 @@ hook.Add("PlayerInitialSpawn", "anti-crash", function(ply)
 	--Console command to help prevent people from crashing.
 	ply:ConCommand("r_drawmodeldecals 0")
 	ply:ConCommand("r_maxmodeldecal 50")
+	
+	--Work around for FCVAR_SERVER_CAN_EXECUTE prevented server running command
+	ply:SendLua("RunConsoleCommand('r_drawmodeldecals', 0)")
+	ply:SendLua("RunConsoleCommand('r_maxmodeldecal', 50)")
 end)
