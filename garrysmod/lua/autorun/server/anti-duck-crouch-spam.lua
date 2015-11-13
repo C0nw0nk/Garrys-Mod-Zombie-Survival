106b93232cae2f25a20aa55e0d24b9b10f76a5e8
C0nw0nk <C0nw0nks@googlemail.com>
2015-11-13 20:44:08 +0000

    Update anti-duck-crouch-spam.lua
    Using network vars.

--- garrysmod/lua/autorun/server/anti-duck-crouch-spam.lua
+++ garrysmod/lua/autorun/server/anti-duck-crouch-spam.lua
@@ -1,21 +1,21 @@
---Allow player to duck
-local allowduck = 1
+--Prevent players from spamming crouch in the air after crouch jumping they will be able to crouch again after they hit the ground.
 --StartCommand allows you to change the players inputs before they are processed by the server.
 hook.Add("StartCommand", "StartCommand-anti-duck-crouch-spam", function(ply, cmd)
 	--If allowduck is 0 then do not allow the player to crouch.
-	if allowduck == 0 then
+	if ply:GetNWInt('allowduck') == 0 then
 		--Remove the ability to crouch.
 		--cmd:RemoveKey(IN_DUCK)
 		--Force the player to stay crouched until the hit the ground.
 		if !ply:IsOnGround() then
 			--Force player to crouch.
 			cmd:SetButtons(IN_DUCK)
+			ply:SendLua("RunConsoleCommand('+duck')")
 		end
 	end
 	--If the player just came out of the crouch animation prevent them being able to crouch again.
 	if ply:KeyReleased(IN_DUCK) then
 		--Prevent player ducking again.
-		allowduck = 0
+		ply:SetNWInt('allowduck', 0)
 	end
 end)
 --OnPlayerHitGround is called when a player makes contact with the ground.
@@ -23,6 +23,9 @@ hook.Add("OnPlayerHitGround", "OnPlayerHitGround-anti-duck-crouch-spam", functio
 	--If the player is on the ground.
 	if player:IsOnGround() then
 		--Allow the player to duck again.
-		allowduck = 1
+		if player:GetNWInt('allowduck') == 0 then
+			player:SendLua("RunConsoleCommand('-duck')")
+		end
+		player:SetNWInt('allowduck', 1)
 	end
 end)
