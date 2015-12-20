bf7ed8bd744980552055477d949b34bcdc432627
C0nw0nk <C0nw0nks@googlemail.com>
2015-12-20 00:15:53 +0000

    Update anti-prop-launch.lua

--- garrysmod/lua/autorun/server/anti-prop-launch.lua
+++ garrysmod/lua/autorun/server/anti-prop-launch.lua
@@ -1,8 +1,11 @@
---Prevent players getting prop killed by the class prop_physics or func_physbox
---You can still prop kill as a zombie.
-hook.Add("EntityTakeDamage", "Anti.Prop.Launch",  function(ent, attacker)
-	local attacker = attacker:GetAttacker()
-	if string.match(attacker:GetClass(), "prop_physics*") or string.match(attacker:GetClass(), "func_physbox*") and ent:IsValid() and ent:IsPlayer() then
-		return true
-	end
+--Make sure the hook runs after all entities are spawned into the map.
+hook.Add("InitPostEntityMap", "InitPostEntityMap.Anti.Prop.Launch", function()
+	--Prevent players getting prop killed by the class prop_physics or func_physbox
+	--You can still prop kill as a zombie.
+	hook.Add("EntityTakeDamage", "Anti.Prop.Launch",  function(ent, attacker)
+		local attacker = attacker:GetAttacker()
+		if string.match(attacker:GetClass(), "prop_physics*") or string.match(attacker:GetClass(), "func_physbox*") and ent:IsValid() and ent:IsPlayer() then
+			return true
+		end
+	end)
 end)
