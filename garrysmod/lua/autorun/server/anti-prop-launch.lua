096b61b640423457ed310e1d70bc43dc3121932c
C0nw0nk <C0nw0nks@googlemail.com>
2016-01-28 04:49:51 +0000

    Update anti-prop-launch.lua

--- garrysmod/lua/autorun/server/anti-prop-launch.lua
+++ garrysmod/lua/autorun/server/anti-prop-launch.lua
@@ -1,7 +1,7 @@
 --Make sure the hook runs after all entities are spawned into the map.
 hook.Add("InitPostEntityMap", "InitPostEntityMap.Anti.Prop.Launch", function()
 	--Timer to force waiting for props to spawn in.
-	timer.Simple(10, function()
+	timer.Simple(1, function()
 		--Prevent players getting prop killed by the class prop_physics or func_physbox
 		--You can still prop kill as a zombie.
 		hook.Add("EntityTakeDamage", "Anti.Prop.Launch",  function(ent, attacker)
