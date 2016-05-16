c94f9fb3ddf4b1760dbf6f3cb3dce062c612a3a5
C0nw0nk <C0nw0nks@googlemail.com>
2016-05-16 12:26:13 +0100

    Update anti-wraith.lua
    Fix white space and add closing bracket to end statement.

--- garrysmod/lua/autorun/server/anti-wraith.lua
+++ garrysmod/lua/autorun/server/anti-wraith.lua
@@ -2,28 +2,28 @@
 --Now the server controls wraith visibility.
 
 hook.Add("StartCommand", "StartCommand-ZombieSurvival-Anti-Wraith-Hacking", function(ply,cmd)
-  --If player is on zombie team and class is wraith.
-  if ply:IsPlayer() and ply:Team() == TEAM_UNDEAD and ply:GetZombieClassTable().Name == "Wraith" then
-    --If the wraith is attacking. (Melee)
-	  if ply:GetActiveWeapon():IsAttacking() then
-	    --Set the wraith to be visible.
-	  	if ply:GetNoDraw() then
-	  		ply:SetNoDraw(false)
+	--If player is on zombie team and class is wraith.
+	if ply:IsPlayer() and ply:Team() == TEAM_UNDEAD and ply:GetZombieClassTable().Name == "Wraith" then
+		--If the wraith is attacking. (Melee)
+		if ply:GetActiveWeapon():IsAttacking() then
+			--Set the wraith to be visible.
+			if ply:GetNoDraw() then
+				ply:SetNoDraw(false)
 			end
 		else
-		  --If the wraith is moving faster than (IN_SPEED)
+			--If the wraith is moving faster than (IN_SPEED)
 			if ply:GetVelocity():Length() > 50 then
-			  --Set the wraith to be visible.
+				--Set the wraith to be visible.
 				if ply:GetNoDraw() then
 					ply:SetNoDraw(false)
 				end
 			--Else if the wraith is walking slowly or (IN_SPEED)
 			elseif ply:GetVelocity():Length() <= 50 then
-			  --Set the wraith to be invisible.
+				--Set the wraith to be invisible.
 				if !ply:GetNoDraw() then
 					ply:SetNoDraw(true)
 				end
 			end
 		end
 	end
-end
+end)
